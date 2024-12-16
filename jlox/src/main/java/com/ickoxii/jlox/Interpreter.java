75cd42a0f8d092e4978c1c0a24c581d8c460f030
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-12-16 06:29:46 -0600

    Inheritance!

--- jlox/src/main/java/com/ickoxii/jlox/Interpreter.java
+++ jlox/src/main/java/com/ickoxii/jlox/Interpreter.java
@@ -92,10 +92,29 @@ class Interpreter implements Expr.Visitor<Object>,
    *
    * This two-stage variable binding allows references to the
    * class inside its own methods.
+   *
+   * We also store a reference to the superclass (if it exists)
+   * and create LoxFunctions for each method. Those capture the
+   * current environment - the one where we just bound "super"
+   * as their closure, which holds on to the superclass.
    * */
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    Object superclass = null;
+    if (stmt.superclass != null) {
+      superclass = evaluate(stmt.superclass);
+      if (!(superclass instanceof LoxClass)) {
+        throw new RuntimeError(stmt.superclass.name,
+            "Superclass must be a class.");
+      }
+    }
+
     environment.define(stmt.name.lexeme, null);
+
+    if (stmt.superclass != null) {
+      environment = new Environment(environment);
+      environment.define("super", superclass);
+    }
 
     /**
      * Turn the syntactic representation of the class (AST node) into
@@ -109,7 +128,13 @@ class Interpreter implements Expr.Visitor<Object>,
       methods.put(method.name.lexeme, function);
     }
 
-    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
+    LoxClass klass = new LoxClass(stmt.name.lexeme,
+        (LoxClass)superclass, methods);
+
+    if (superclass != null) {
+      environment = environment.enclosing;
+    }
+
     environment.assign(stmt.name, klass);
     return null;
   }
@@ -154,6 +179,34 @@ class Interpreter implements Expr.Visitor<Object>,
     Object value = evaluate(expr.value);
     ((LoxInstance)object).set(expr.name, value);
     return value;
+  }
+
+  /**
+   * Looks up the surrounding class's superclass by looking
+   * up "super" in the proper environment.
+   *
+   * When accessing a method, we need to bind `this` to the
+   * object the method is accessed from. Even though we are
+   * looking up the method on the superclass, the _instance_
+   * is still `this`.
+   * */
+  @Override
+  public Object visitSuperExpr(Expr.Super expr) {
+    int distance = locals.get(expr);
+    LoxClass superclass = (LoxClass)environment.getAt(
+        distance, "super");
+
+    LoxInstance object = (LoxInstance)environment.getAt(
+        distance - 1, "this");
+
+    LoxFunction method = superclass.findMethod(expr.method.lexeme);
+
+    if (method == null) {
+      throw new RuntimeError(expr.method,
+          "Undefine property '" + expr.method.lexeme + "'.");
+    }
+
+    return method.bind(object);
   }
 
   @Override
--- jlox/src/main/java/com/ickoxii/jlox/LoxClass.java
+++ jlox/src/main/java/com/ickoxii/jlox/LoxClass.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 class LoxClass implements LoxCallable {
   final String name;
+  final LoxClass superclass;
   private final Map<String, LoxFunction> methods;
 
   /**
    * Instance stores state, class stores behavior
    * */
-  LoxClass(String name, Map<String, LoxFunction> methods) {
+  LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
+    this.superclass = superclass;
     this.name = name;
     this.methods = methods;
   }
@@ -18,6 +20,10 @@ class LoxClass implements LoxCallable {
   LoxFunction findMethod(String name) {
     if (methods.containsKey(name)) {
       return methods.get(name);
+    }
+
+    if (superclass != null) {
+      return superclass.findMethod(name);
     }
 
     return null;
